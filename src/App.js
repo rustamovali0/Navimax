391f4fd69ef318e6709ff2864562b5448759774c
rustamovali0 <rustamovali012@gmail.com>
2024-06-10 12:14:27 +0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import Home from './Components/Pages/Home';
+import Features from './Components/Pages/Features';
+import Testimonials from './Components/Pages/Testimonials';
+import Blog from './Components/Pages/Blog';
+import Contact from './Components//Pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route  path="/Features" element={<Features />} />
+            <Route path="/Testimonials" element={<Testimonials />} />
+            <Route path="/Blog" element={<Blog />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
