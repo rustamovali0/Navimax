6d0fe2c03ac4a2763058b0dceb11282b666d12db
rustamovali0 <rustamovali012@gmail.com>
2024-06-10 12:25:08 +0400

    Added_Img

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-     
+    <div className="Navbar">
+      <div className="logo">{/* <img src="" alt="" /> */}</div>
+      <div className="routes">
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Testimonials</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
