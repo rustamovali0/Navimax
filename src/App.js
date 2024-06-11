fac1d90a28106d1fd0022772419e86c1b9422385
rustamovali0 <rustamovali012@gmail.com>
2024-06-11 10:18:00 +0400

    Second

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Home from './Components/Pages/Home';
 import Features from './Components/Pages/Features';
 import Testimonials from './Components/Pages/Testimonials';
 import Blog from './Components/Pages/Blog';
-import Contact from './Components//Pages/Contact';
+import Contact from './Components/Pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route  path="/Features" element={<Features />} />
-            <Route path="/Testimonials" element={<Testimonials />} />
-            <Route path="/Blog" element={<Blog />} />
-            <Route path="/Contact" element={<Contact />} />
-          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/Features" element={<Features />} />
+          <Route path="/Testimonials" element={<Testimonials />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom"; 
+import MobiLogo from "../Asserts/Mobi_Logo.png";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const navItems = [
+    { path: "/Home", label: "Home" },
+    { path: "/Features", label: "Features" },
+    { path: "/Testimonials", label: "Testimonials" },
+    { path: "/Blog", label: "Blog" },
+    { path: "/Contact", label: "Contact" }
+  ];
+
   return (
     <div className="Navbar">
-      <div className="logo"><img src="" alt="" /></div>
-      <div className="routes">
-        <ul>
-          <li>Home</li>
-          <li>Features</li>
-          <li>Testimonials</li>
-          <li>Blog</li>
-          <li>Contact</li>
-        </ul>
+      <div className="logo">
+        <Link to="/Home">
+          <img src={MobiLogo} alt="Mobi Logo" />
+        </Link>
       </div>
+      <ul className="NavUl">
+        {navItems.map((item) => (
+          <li
+            key={item.path}
+            className={location.pathname === item.path ? "active" : ""}
+          >
+            <Link to={item.path}>
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
