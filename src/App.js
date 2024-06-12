e57d43b7b9b098d752f2c6ba2b6688ae67de35a8
rustamovali0 <rustamovali012@gmail.com>
2024-06-12 23:23:58 +0400

    rgtyuk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-import Home from './Components/Pages/Home';
-import Features from './Components/Pages/Features';
-import Testimonials from './Components/Pages/Testimonials';
-import Blog from './Components/Pages/Blog';
-import Contact from './Components/Pages/Contact';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Features from "./Components/Pages/Features";
+import Testimonials from "./Components/Pages/Testimonials";
+import Blog from "./Components/Pages/Blog";
+import Contact from "./Components/Pages/Contact";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Features" element={<Features />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Features" element={<Features />} />
           <Route path="/Testimonials" element={<Testimonials />} />
           <Route path="/Blog" element={<Blog />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Mobi_Logo from '../Asserts/Mobi_Logo.png'
+const Fotter = () => {
+  return (
+    <div>
+     <div className="Footer">
+  <img src={Mobi_Logo} alt="Mobi Logo" className="footer-logo" />
+  <div className="iconsFooter">
+    A A A
+  </div>
+  <form className="footer-form">
+    <input type="email" name="email" id="emailInput" placeholder="Enter your email" />
+    <input type="submit" value="Sign Up" />
+  </form>
+</div>
+
+    </div>
+  )
+}
+
+export default Fotter
