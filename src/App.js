26dded21a9397256f44228009526b9629d60407d
rustamovali0 <rustamovali012@gmail.com>
2024-06-14 14:06:38 +0400

    6/14.2024

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
